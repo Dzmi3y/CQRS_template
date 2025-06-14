f389e54f26651b40ee3447534c38d8cb36dc9e6d
Dzmitry <dzmi3y@gmail.com>
2025-06-14 15:11:13 +0300

    Added decoration and background  to HomePage main block

---
+++ ct.ui/src/pages/Home/Index.tsx
@@ -0,0 +1,11 @@
+import MainBlock from "./MainBlock/MainBlock";
+
+const HomePage = () => {
+  return (
+    <>
+      <MainBlock />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ ct.ui/src/pages/Home/MainBlock/MainBlock.tsx
@@ -0,0 +1,12 @@
+import styles from "./styles.module.css";
+import BottomWaveImage from "../../../assets/images/decorations/Bottom_wave.svg";
+
+const MainBlock = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.bottomWave} />
+    </div>
+  );
+};
+
+export default MainBlock;
