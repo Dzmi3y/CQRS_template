a1aeef7175d25e30d6e9b4bf92ff879e7bf39d31
Dzmitry <dzmi3y@gmail.com>
2025-06-16 17:15:25 +0300

    Added adaptive text

--- ct.ui/src/pages/Home/MainBlock/MainBlock.tsx
+++ ct.ui/src/pages/Home/MainBlock/MainBlock.tsx
@@ -1,10 +1,12 @@
 import styles from "./styles.module.scss";
-import BottomWaveImage from "../../../assets/images/decorations/Bottom_wave.svg";
+import BottomWaveImage from "@images/decorations/Bottom_wave.svg";
 
 const MainBlock = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.bottomWave} />
+      <div>test</div>
+      <div className={styles.welcomeText}>Welcome ın our store</div>
+      <img className={styles.bottomWave} src={BottomWaveImage} alt="" />
     </div>
   );
 };
--- ct.ui/vite.config.ts
+++ ct.ui/vite.config.ts
@@ -1,10 +1,17 @@
-import {defineConfig} from 'vite';
-import plugin from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import plugin from "@vitejs/plugin-react";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 59710,
-    }
-})
+  plugins: [plugin()],
+  server: {
+    port: 59710,
+  },
+  resolve: {
+    alias: {
+      "@mixins": path.resolve(__dirname, "src/mixins"),
+      "@images": path.resolve(__dirname, "src/assets/images"),
+    },
+  },
+});
